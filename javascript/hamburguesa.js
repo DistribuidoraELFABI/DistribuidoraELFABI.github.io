2dc7b8a7cca59534dbc941a61ec0c4228141c315
DistribuidoraELFABI <distribuidoraalpos@hotmail.com>
2025-02-20 12:12:28 -0500

    Update hamburguesa.js

--- javascript/hamburguesa.js
+++ javascript/hamburguesa.js
@@ -1,22 +1,42 @@
-function hamburguerMenu(panelBtn, panel){
+function hamburguerCart(panelBtn, panel){
     const d = document;
-    d.addEventListener("click",e=>{
-        if(e.target.matches(panelBtn)|| e.target.matches(`${panelBtn} *`)){
+    d.addEventListener("click", (e) => {
+        if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
             d.querySelector(panel).classList.toggle("is-active");
             d.querySelector(panelBtn).classList.toggle("is-active");
         }
-        if(e.target.matches(menuLink)){
-            d.querySelector(panel).classList.remove("is-active");
-            d.querySelector(panelBtn).classList.remove("is-active");
-
-
-        }
-    })
+        
+        // Cierra el menú si se hace clic en un enlace dentro de él
+        // if (e.target.closest(panel)) {
+        //     d.querySelector(panel).classList.remove("is-active");
+        //     d.querySelector(panelBtn).classList.remove("is-active");
+        // }
+    });
 }
 
-const d=document;
-d.addEventListener("DOMContentLoaded",(e)=>{
-    hamburguerMenu(".panelBtn", ".carrito-detalles",".carrito-detalles a")
-})
+document.addEventListener("DOMContentLoaded", () => {
+    hamburguerCart(".panelBtn", ".carrito-detalles"); // Para el carrito de compras
+});
 
+function hamburguerMenu(menuBtn, panel) {
+    const d = document;
+    
+    d.addEventListener("click", (e) => {
+        if (e.target.matches(menuBtn) || e.target.matches(`${menuBtn} *`)) {
+            d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(menuBtn).classList.toggle("is-active");
+
+        } 
+        
+        // Cierra el menú si se hace clic fuera de él
+        if (e.target.closest(panel) ) {
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(menuBtn).classList.remove("is-active");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    hamburguerMenu("#menu1", ".menu-hamburguesa"); // Para el menú en el footer
+});
 
